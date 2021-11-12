edb81de3ca55abf62a2cbc93f72bea8876e6325f
nandopuntodewo <89438064+nandopuntodewo@users.noreply.github.com>
2021-11-12 23:15:01 +0700

    Update delivery.cpp

--- delivery.cpp
+++ delivery.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
 using namespace std;
 
-//PROJECT UNNES DELIVERY
-struct main(){
-  
-  
-};
+//PROJECT UNNES DELVERY
+int main(){
+	int pilihan;
+	char confirm;
+do
+{
+    cout << “PUNTEN DELIVERY” << endl;
+    cout << “================” << endl;
+    cout << “ Selamat datang di tampilan utama Punten Delivery” << endl;
+    cout << endl;
+    cout << “##  Daftar Layanan Pengiriman Punten Delivery  ##” << endl;
+    cout << “=======================================” << endl;
+    cout << “ 1. Pengiriman Standar ( 2 - 4 Hari )” << endl;
+    cout << “ 2. Pengiriman Express ( 1 - 2 Hari)” << endl;
+    cout << endl;
+
+    cout << “Pilihan anda : “;
+    Cin >> pilihan;
+
+    switch(pilihan){
+    case 1:
+      cout << “Anda memilih pengiriman standar” << endl;
+      break;
+    case 2:
+      cout  << “Anda memilih pengiriman express” << endl;
+      break;
+    default :
+      cout << “Pilihan tidak tersedia” << endl;
+    }
+    cout << endl;
+
+    cout << “Pilihan anda sudah benar (y/n)? “;
+    cin >> confirm;
+    cout << endl;
+}
+while (confirm != ‘n’);
